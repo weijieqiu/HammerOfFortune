92f5f2a1a033add4d4d4dc711167c11cd528a58e
weijieqiu <qiuweijiecn@foxmail.com>
2020-06-07 00:00:23 +0800

    修改页面回显数据获取失败
    修改页面回显数据获取失败

--- pages/one/one.js
+++ pages/one/one.js
@@ -5,13 +5,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-      verifyBtnText: '点我',
-      storageData:'',
-  //验证码倒计时
+    verifyBtnText: '点我',
+    storageData: '',
+    //验证码倒计时
     verifyBtnDisable: false,
-    member:'',
-    oneMember:'',
-    i:0
+    member: '',
+    oneMember: '',
+    i: 0
 
   },
 
@@ -26,75 +26,92 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-      var that = this;
-      wx.getStorage({
-          key: 'user',
-          success(res) {
-              console.log(res.data)
-              let newStorageData =res.data;
-              that.setData({
-                  storageData: newStorageData,
-              })
-          }
-      })
-      let num = 2;
-      let member1 = this.data.storageData;
-      function randomArrByOut(arr) {
-        　　for (var i = arr.length - 1; i >= 0; i--) {
-        　　　　var randomIndex = Math.floor(Math.random() * (i + 1));
-        　　　　var itemAtIndex = arr[randomIndex];
-        　　　　arr[randomIndex] = arr[i];
-        　　　　arr[i] = itemAtIndex;
-        　　}
-        　　return arr;
+    var that = this;
+    wx.getStorage({
+      key: 'user',
+      success(res) {
+        console.log(res.data)
+        let newStorageData = res.data;
+        that.setData({
+          storageData: newStorageData,
+        })
       }
-      /**
-       * 由于randomArrByOut随机的不太明显内部让他先随机五次显得明显
-       */
-      let randomMember =randomArrByOut(member1);
-      this.setData({
-        member: randomMember
-      })
-      // let interval = setInterval(e => {
-      //   if (num <= 0) {
-      //     clearInterval(interval);
-      //     this.setData({
-      //       verifyBtnText: "锤我",
-      //       verifyBtnDisable: false,
-      //       member: randomMember
-      //     });
-  
-      //   } else {
-  
-      //     num--;
-  
-      //     this.setData({
-      //       verifyBtnText: num + 's',
-      //       verifyBtnDisable: true,
-      //       member:""
-  
-      //     });
-  
-      //   }
-  
-      // }, 500)
+    })
+    let num = 2;
+    let member1 = this.data.storageData;
+    function randomArrByOut(arr) {
+      for (var i = arr.length - 1; i >= 0; i--) {
+        var randomIndex = Math.floor(Math.random() * (i + 1));
+        var itemAtIndex = arr[randomIndex];
+        arr[randomIndex] = arr[i];
+        arr[i] = itemAtIndex;
+      }
+      return arr;
+    }
+    /**
+     * 由于randomArrByOut随机的不太明显内部让他先随机五次显得明显
+     */
+    let randomMember = randomArrByOut(member1);
+    this.setData({
+      member: randomMember
+    })
+    // let interval = setInterval(e => {
+    //   if (num <= 0) {
+    //     clearInterval(interval);
+    //     this.setData({
+    //       verifyBtnText: "锤我",
+    //       verifyBtnDisable: false,
+    //       member: randomMember
+    //     });
+
+    //   } else {
+
+    //     num--;
+
+    //     this.setData({
+    //       verifyBtnText: num + 's',
+    //       verifyBtnDisable: true,
+    //       member:""
+
+    //     });
+
+    //   }
+
+    // }, 500)
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-      var that = this;
-      wx.getStorage({
-          key: 'user',
-          success(res) {
-              console.log(res.data)
-              let newStorageData =res.data;
-              that.setData({
-                  storageData: newStorageData,
-              })
+    var that = this;
+    wx.getStorage({
+      key: 'user',
+      success(res) {
+        console.log(res.data)
+        let newStorageData = res.data;
+        let num = 2;
+        let member1 = newStorageData;
+        function randomArrByOut(arr) {
+          for (var i = arr.length - 1; i >= 0; i--) {
+            var randomIndex = Math.floor(Math.random() * (i + 1));
+            var itemAtIndex = arr[randomIndex];
+            arr[randomIndex] = arr[i];
+            arr[i] = itemAtIndex;
           }
-      })
+          return arr;
+        }
+        /**
+         * 由于randomArrByOut随机的不太明显内部让他先随机五次显得明显
+         */
+        let randomMember = randomArrByOut(member1);
+        that.setData({
+          storageData: newStorageData,
+          member: randomMember
+        })
+        console.log(that.data.member)
+      }
+    })
   },
 
   /**
@@ -134,20 +151,20 @@ Page({
   countDown: function () {
     console.log(this.data.member)
     let that = this;
-    if(that.data.i < this.data.member.length){
+    if (that.data.i < this.data.member.length) {
       this.setData({
         oneMember: that.data.member[that.data.i]
       })
-    }else{
+    } else {
       this.setData({
         oneMember: '数据已取完,可重新进入'
       })
     }
     this.data.i++;
   },
-  gotoOpeartion: function(){
-      wx.navigateTo({
-        url: '../../pages/operation/operation',
-      })
-    }
+  gotoOpeartion: function () {
+    wx.navigateTo({
+      url: '../../pages/operation/operation',
+    })
+  }
 })
